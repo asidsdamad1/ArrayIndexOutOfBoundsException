2984aab75337e3901fab961d9c187487cbbb6ec1
trunghieu <trunghieu.ngo612@gmail.com>
2021-01-06 22:38:11 +0700

    .

---
+++ src/ArrayExample.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.Random;
+
+public class ArrayExample {
+    public static int[] array() {
+        int arr[] = {};
+        return arr;
+    }
+
+    public Integer[] createRandom() {
+        Random rd = new Random();
+        Integer[] arr = new Integer[100];
+        System.out.println("Danh sách phần tử của mảng: " );
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rd.nextInt(100);
+            System.out.print(arr[i]+ " ");
+        }
+        return arr;
+    }
+}
